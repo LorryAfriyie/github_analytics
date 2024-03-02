4351a93407464481bc672578adc7727b4a8b57ec
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-03-02 04:38:46 +0200

    Setting up octokit
    Installing octokit

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,51 @@
+import { useEffect, useState } from "react";
+
 export const Card = () => {
-  const greetings: string = "Hello, World!";
+  const [gitFollowers, getGitFollowers] = useState([]);
+  const [gitFollowing, getGitFollowing] = useState([]);
+
+  const followers = () => {
+    fetch(
+      `https://api.github.com/users/${import.meta.env.VITE_USERNAME}/followers`,
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        getGitFollowers(data);
+        console.log("Followers");
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const following = () => {
+    fetch(
+      `https://api.github.com/users/${import.meta.env.VITE_USERNAME}/following`,
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        getGitFollowing(data);
+        console.log("Following");
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    followers();
+  }, []);
 
   return (
     <div className="card">
       <div className="card-header"></div>
-      <div className="card-body">
-        <p>{greetings}</p>
-      </div>
+      <div className="card-body">{import.meta.env.VITE_GREETING}</div>
       <div className="card-footer"></div>
     </div>
   );
